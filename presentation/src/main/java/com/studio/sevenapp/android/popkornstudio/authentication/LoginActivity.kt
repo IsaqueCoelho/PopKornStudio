8df54ed32bb7b78084e5a96c51025b17222ca08f
Isaque Coelho <isaque.coelho12@gmail.com>
2020-05-15 21:13:54 -0300

    Changes on,
    ## Config
     - Implemented Firebase module and changed domain to android module;

---
+++ presentation/src/main/java/com/studio/sevenapp/android/popkornstudio/authentication/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.studio.sevenapp.android.popkornstudio.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.studio.sevenapp.android.popkornstudio.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
